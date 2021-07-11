9f050cc6ae34136d3cf274620b7284e4515c79bd
takkyuuplayer <takkyuuplayer@gmail.com>
2021-07-11 18:29:25 +0900

    再帰下降構文解析

--- 9cc.c
+++ 9cc.c
@@ -27,6 +27,7 @@ typedef enum {
 } TokenKind;
 
 typedef struct Token Token;
+
 struct Token {
   TokenKind kind;
   Token *next;
@@ -92,6 +93,77 @@ Token *tokenize(char *p) {
   }
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // int
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *primary();
+Node *mul();
+
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
+Node *mul() {
+  Node *node = primary();
+  for (;;) {
+    if (consume('*')) {
+      node = new_node(ND_MUL, node, primary());
+    } else if (consume('/')) {
+      node = new_node(ND_DIV, node, primary());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node *expr() {
+  Node *node = mul();
+  for (;;) {
+    if (consume('+')) {
+      node = new_node(ND_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_node(ND_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
 }
 
 int main(int argc, char **argv) {
