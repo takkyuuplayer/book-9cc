2a769565419ea73d28fc9f90e5d7bf0a06d0367d
takkyuuplayer <takkyuuplayer@gmail.com>
2021-07-11 18:59:45 +0900

    スタックマシンまで

--- 9cc.c
+++ 9cc.c
@@ -166,6 +166,38 @@ Node *expr() {
   }
 }
 
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("	push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("	pop rdi\n");
+  printf("	pop rax\n");
+  switch (node->kind) {
+  case ND_ADD:
+    printf("	add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("	sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("	imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("	cqo\n");
+    printf("	idiv rdi\n");
+    break;
+  case ND_NUM:
+    break;
+  }
+
+  printf("	push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "The number of arguments must be two.");
